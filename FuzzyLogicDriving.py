a5b68b350ef06cce4bedf86f1c1cf5e95c1668e2
unknown <anmil25@student.sdu.dk>
2025-11-26 10:30:44 +0100

    comments added

--- FuzzyLogicDriving.py
+++ FuzzyLogicDriving.py
@@ -38,7 +38,7 @@ max_delay_us=1000
 min_delay_us=500
 move_unit="dist"
 
-#turnmode="curve"
+#turnmode="curve" # experimental variable
 
 # Generel driving class calls
 turnLeft=DifferentialDrive(left, right, stepmode, microsteps_per_step, pwm_pct, "l", turnmode, frequency)
@@ -46,33 +46,37 @@ turnRight=DifferentialDrive(left, right, stepmode, microsteps_per_step, pwm_pct,
 straight=DifferentialDrive(right, left, stepmode, microsteps_per_step, pwm_pct, "N/A", turnmode, frequency)
 
 # Speed mode parameters
-delay_us=max_delay_us
+delay_us=max_delay_us # Start in slow mode
 speed_mode_vis = Pin(13, Pin.OUT)
 speed_mode_vis.value(0)
 
+# Emergency stop parameters
 emergencyStop = Pin(14, Pin.IN, Pin.PULL_DOWN)
 emergency_stop = 0
 
+# Define emergency stop function
 def button_check(pin):
     if emergencyStop.value() == 1:   # Activate if button is pressed
         global emergency_stop
         print("Emergency stop initiated")
         time.sleep(0.3)   # Debounce
-        speed_mode_vis.value(0)
-        emergency_stop = 1
+        speed_mode_vis.value(0) # Turn off speed-mode-visualisation-LED
+        emergency_stop = 1 # Change value of main loop dependant variable to exit main loop
 
 time.sleep(1)
 
+# Call in emergency stop as an interrupt
 emergencyStop.irq(button_check)   # Call in the emergency button check
 
+# Main loop
 while emergency_stop == 0:
-    ReadVoltage()
+    ReadVoltage() # Check button state
     #time.sleep(0.1)
     #print("active")
     # Speed mode change
-    if voltagey == 3.3 and (1 < voltagex < 2): # Check if joystick button is pressed
+    if voltagey == 3.3 and (1 < voltagex < 2): # React if joystick button is pressed
         time.sleep(0.2) # Debounce
-        Pin.toggle(speed_mode_vis)
+        Pin.toggle(speed_mode_vis) # Toggle LED on/off
         #print(speed_mode_vis.value())
         if speed_mode_vis.value() == 0: # Activate slow mode
             delay_us = max_delay_us
@@ -84,12 +88,13 @@ while emergency_stop == 0:
         turnLeft.move(dist_turn, direction, delay_us, move_unit)
     elif voltagex > 2 and (1 < voltagey < 2): # Joystick is turned to the Right
         turnRight.move(dist_turn, direction, delay_us, move_unit)
-    elif (3.3 > voltagey > 2) and (1 < voltagex < 2): # Joystick is turned backward and button is not pressed
+    elif (3.3 > voltagey > 2) and (1 < voltagex < 2): # Joystick is turned backward and joystick button is not pressed
         straight.move(dist_straight, "backward", delay_us, move_unit)
     elif voltagey < 1 and (1 < voltagex < 2): # Joystick is turned forward
         straight.move(dist_straight, "forward", delay_us, move_unit)
     else: # Joystick is untouched
         pass
 
+# Stop all motors and confirm full stop
 straight.stop()
 print("Succesfully stopped")
