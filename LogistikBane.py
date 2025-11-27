65c2bb311089eaf67223a361518a777a00d0bcb8
Your Name <you@example.com>
2025-11-27 17:09:34 +0100

    udvikling af statemachine

--- LogistikBane.py
+++ LogistikBane.py
@@ -8,25 +8,79 @@ magnet = Electromagnet()
 #magnet.start()
 #magnet.stop()
 
-#State
-count = 0
+#States
+def nut1():
+    global count
+    Drive.singlecheck()
+    if voltageR >= ?:
+        return # exit function early 
+    #turn to spot
+    Drive.rightturnrobot
+    magnet.start()
+    #turn front to start
+    Drive.rightturnrobot
+    count += 1
+        
+def home1() 2: ? #return home
+    global count
+    Drive.singlecheck()
+    if voltageR < ?: return
+    if voltageM < ?: return
+    if voltageL < ?: return
+    magnet.stop()
+    Drive.rightturnrobot
+    Drive.rightturnrobot
+    count += 1
+    
+
+def nut2():
+    global count
+    Drive.singlecheck()
+    if voltageL >= ? return
+    Drive.leftturnrobot
+    magnet.start
+    Drive.leftturnrobot
+    count +=1
+
+def home2():
+    global count
+    Drive.singlecheck()
+    if voltageR < ?: return
+    if voltageM < ?: return
+    if voltageL < ?: return
+    magnet.stop()
+    Drive.leftturnrobot
+    Drive.leftturnrobot
+
+#def nut3()
+
+
+count = 1
+
 if voltageM or voltageL <?:
     count +=1
 
-#state 1
-if count = 0
+while True:    #state 1
+    while count == 1:
+        nut1()
 
-#state 2
-elif count = 1
+    #state 2
+    while count == 2:
+        home1()
 
-#state 3
-elif count = 2
+    #state 3
+    while count == 3:
+        nut2()
 
-#state 4
-elif count = 3
+    #state 4
+    while count == 4:
+        home2()
+    
+    
+"""
+    #state 5
+    while count == 5
 
-#state 5
-elif count = 4
+    #state 6
+    while count == 5
 
-#state 6
-elif count = 5
