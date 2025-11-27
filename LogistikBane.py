71f244c9cde909531aeed24e7b748134cf68f0eb
Your Name <you@example.com>
2025-11-27 15:43:57 +0100

    actual first try for at statemachine

--- LogistikBane.py
+++ LogistikBane.py
@@ -1,4 +1,32 @@
-from LDR_Turn_ReactionClassV2 import TrackDriving
+from LDR_Turn_ReactionClassV3 import TrackDriving
+from MagnetClass import Electromagnet 
 
 Drive = TrackDriving()
-Drive.runrobot()
+#Drive.runrobot()
+
+magnet = Electromagnet()
+#magnet.start()
+#magnet.stop()
+
+#State
+count = 0
+if voltageM or voltageL <?:
+    count +=1
+
+#state 1
+if count = 0
+
+#state 2
+elif count = 1
+
+#state 3
+elif count = 2
+
+#state 4
+elif count = 3
+
+#state 5
+elif count = 4
+
+#state 6
+elif count = 5
