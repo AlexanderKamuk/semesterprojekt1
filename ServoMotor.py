e5ab6e64c5c10deb8b5aac65036bf103f0482c14
Your Name <you@example.com>
2025-12-03 20:45:33 +0100

    servo class

--- ServoMotor.py
+++ ServoMotor.py
@@ -1,23 +1,25 @@
 from machine import Pin, PWM
 import time
 
-#initializes GPIO16 as the pico pin for the servo motor, and sets the frequency to 50 hz
-servo_pin=("GP16")
-servo=PWM(servo_pin)
-servo.freq(50)
+class ServoMotor:
+    def __init__(self):
+        #initializes GPIO16 as the pico pin for the servo motor, and sets the frequency to 50 hz
+        self.servo_pin=Pin(16)
+        self.servo=PWM(self.servo_pin)
+        self.servo.freq(50)
 
 
+    def DegreeServo(self):
+            #calculates the positions for the servo.(regulated through testing)
+        self.duty=65535
+        self.deg_0	= int(0.5 / 20 * self.duty)
+        self.deg_90	= int(1.55 / 20 * self.duty)
+        self.deg_180	= int(2.5 / 20 * self.duty)
 
-#calculates the positions for the servo.(regulated through testing)
-duty=65535
-deg_0	= int(0.5 / 20 * duty)
-deg_90	= int(1.55 / 20 * duty)
-deg_180	= int(2.5 / 20 * duty)
-
-#Moves the servo to the given positions
-servo.duty_u16(deg_0)
-time.sleep(2)
-servo.duty_u16(deg_90)
-time.sleep(2)
-servo.duty_u16(deg_180)
-time.sleep(2)
+    #Moves the servo to the given positions
+    def servo0deg(self):
+        self.servo.duty_u16(self.deg_0)
+    def servo90deg(self):
+        self.servo.duty_u16(self.deg_90)
+    def servo180deg(self):
+        self.servo.duty_u16(self.deg_180)
